1f1c179d79c86f5a59341e644691702694b22425
bird2008 <orzechowskib2008@gmail.com>
2021-12-04 14:01:47 +0100

    Add valiodation

--- ex2.py
+++ ex2.py
@@ -8,23 +8,45 @@ d = 0
 
 try:
     val = int(a)
-    c = int(c) + int(a)
-    d = int(d) + int(b)
-    while c != d:
-        it2 += 1
-        if c > d:
-            c = c - d
-        else:
-            d = d - c     
-    while int(b) > 0:
-        it1 += 1
-        r = int(a) % int(b)
-        a = b
-        b = r
-    nwd = a
 except ValueError:
-    print("Wejście musi być liczbą!")
+    print("Podaj liczby obie ujemne lub obie dodatnie, całkowite, różne od 0!")
+    quit()
 
+if int(a) > 0 and int(b) < 0: 
+    print("Podaj liczby obie ujemne lub obie dodatnie, całkowite, różne od 0!")
+    quit()
+
+if int(b) > 0 and int(a) < 0: 
+    print("Podaj liczby obie ujemne lub obie dodatnie, całkowite, różne od 0!")
+    quit()
+
+if int(a) < 0 and int(b) < 0:
+    a = int(a)
+    a = abs(a)
+    b = int(b)
+    b = abs(b)
+
+if int(a) == 0  or int(b) == 0:
+    print("Podaj liczby obie ujemne lub obie dodatnie, całkowite, różne od 0!")
+    quit()
+    
+c = int(c) + int(a)
+d = int(d) + int(b)
+while c != d:
+    it2 += 1
+    if c > d:
+        c = c - d
+    else:
+        d = d - c     
+while int(b) > 0:
+    it1 += 1
+    r = int(a) % int(b)
+    a = b
+    b = r
+
+nwd = a
+    
 print("NWD = ", int(nwd))
 print("L_iteracji (I metoda) = {}".format(it1))
 print("L_iteracji (II metoda) = {}".format(it2))
+quit()
