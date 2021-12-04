ef19a699c0a8e251b437c7aabd2711bc2b7f6c2d
bird2008 <80196280+bird2008@users.noreply.github.com>
2021-12-04 11:18:32 +0100

    Change try except

--- ex2.py
+++ ex2.py
@@ -8,23 +8,26 @@ d = 0
 
 try:
     val = int(a)
-    c = int(c) + int(a)
-    d = int(d) + int(b)
-    while c != d:
-        it2 += 1
-        if c > d:
-            c = c - d
-        else:
-            d = d - c     
-    while int(b) > 0:
-        it1 += 1
-        r = int(a) % int(b)
-        a = b
-        b = r
-    nwd = a
 except ValueError:
     print("Wejście musi być liczbą!")
     quit()
+    
+c = int(c) + int(a)
+d = int(d) + int(b)
+while c != d:
+    it2 += 1
+    if c > d:
+        c = c - d
+    else:
+        d = d - c   
+
+while int(b) > 0:
+    it1 += 1
+    r = int(a) % int(b)
+    a = b
+    b = r
+    
+nwd = a    
 
 print("NWD = ", int(nwd))
 print("L_iteracji (I metoda) = {}".format(it1))
