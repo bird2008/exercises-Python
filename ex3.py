6ecfa73610ed13773ed7467d898dfc3e246acdba
bird2008 <80196280+bird2008@users.noreply.github.com>
2021-12-05 22:14:01 +0100

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -13,16 +13,15 @@ except:
 
 if y <= 600 and y >= 0 and skok <= 10 and skok >= 0:
     while y > 0:
+        if skok <= 0:
+            print("Liczba skoków: -1")
+            quit()
         nls += 1
         y -= skok
         c += 1
         if c == 10:
-            if skok > 0:
-                print("Liczba skoków: -1")
-                quit()
-            else: 
-                skok -= 1
-                c = 0 
+            skok -= 1
+            c = 0 
 else:
     print("Podaj szerokość łaki(0 - 600) oraz długość skoku(0 - 10)")
     quit()
