0de1d34595c10da98ac546bc2cd62f351617d005
bird2008 <80196280+bird2008@users.noreply.github.com>
2021-12-05 22:05:48 +0100

    Fix validation

--- ex3.py
+++ ex3.py
@@ -1,7 +1,7 @@
 y = input("y: ")
 skok = input("skok: ")
 
-mls = 0
+nls = 0
 c = 0
 
 try:
@@ -11,19 +11,19 @@ except:
     print("Podaj liczbę o z przedziału 0 do 600!")
     quit()
 
-if y <= 600 and y > 0 and skok <= 10 and skok > 0:
+if y <= 600 and y >= 0 and skok <= 10 and skok >= 0:
     while y > 0:
-        mls += 1
+        nls += 1
         y -= skok
         c += 1
-        if skok < 0:
-            print("Liczba skoków: -1")
-            quit()
         if c == 10:
+            if skok <= 0:
+                print("Liczba skoków: -1")
+                quit()
             skok -= 1
             c = 0 
 else:
     print("Podaj szerokość łaki(0 - 600) oraz długość skoku(0 - 10)")
     quit()
 
-print("Liczba skoków: {}".format(mls))
+print("Liczba skoków: {}".format(nls))
